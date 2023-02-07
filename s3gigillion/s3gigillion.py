c6d926505190604548dd7f4fa62e24541fd67f05
document <amrv.1990@gmail.com>
2023-02-06 22:53:02 -0500

    Update s3gigillion.py

--- s3gigillion/s3gigillion.py
+++ s3gigillion/s3gigillion.py
@@ -1,3 +1,24 @@
 from boto3 import client
 
 SERVICE_ENDPOINT = 'https://s3.gigillion.com'
+
+def get_client(access_key_id,
+               secret_access_key,
+               session_token=None):
+
+    storage = _GigillionStorage(access_key_id, secret_access_key, session_token).storage
+    return storage
+
+class _GigillionStorage:
+
+    def __init__(self,
+                 access_key_id,
+                 secret_access_key,
+                 session_token=None):
+        self.storage = client(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=session_token,
+            endpoint_url=SERVICE_ENDPOINT
+        )
